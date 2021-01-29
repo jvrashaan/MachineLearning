7791f76b5dbe27a831cee8afba0483478a4cad93
Javiar Titus <javiar.titus@amac02w42jxhtd8.fios-router.home>
2021-01-28 23:58:36 -0500

    updates

--- logisticRegressionModels/logisticRegression.py
+++ logisticRegressionModels/logisticRegression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+import math
 
 class logisticRegression():
     #Given a data set of home features and home prices this class will predict the price of homes
@@ -13,4 +14,37 @@ class logisticRegression():
         
         self.data = np.array(re.split(',|\n', data)).reshape((100, 3)).astype(np.float)
 
+    def sigmoid(self, z):
+        #returns a matrix or array where every value is "activated" with the sigmoid function
+        rows, cols = z.shape
+
+        g = np.zeros((rows, cols))
+        for i in range(rows):
+            for j in range(cols):
+                g[i, j] = 1/(1 + math.exp(-z[i, j]))
+        
+        return g
     
+    def costFunction(self, theta, X, Y):
+        #Compute cost and gradient for logistic regression
+        m = len(Y); # number of training examples
+        f = len(X[0])
+        cost = 0
+        grad = np.zeros((theta.size, 1))
+
+        for i in range(m):
+            cost += -Y[i] * math.log(self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1))) - (1 - Y[i]) * np.log(1 - self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1)))
+
+        cost = cost / m
+
+        temp = np.zeros((f, 1))
+
+        for i in range(m):
+            for j in range(f):
+                
+                temp[j, 0] += ((self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1)) - Y[i]) * X[i, j])
+            
+        for i in range(f):
+            grad[i, 0] = temp[i, 0] / m
+        
+        return cost, grad
--- logisticRegressionModels/logisticRegressionTest.py
+++ logisticRegressionModels/logisticRegressionTest.py
@@ -7,6 +7,7 @@ from logisticRegression import logisticRegression
 
 test = logisticRegression()
 #original data plot
+fig = plt.figure(figsize=(10,7))
 positive = [v[:-1] for x, v in enumerate(test.data) if test.data[x][2]]
 negative = [v[:-1] for x, v in enumerate(test.data) if test.data[x][2] == 0]
 plt.scatter([v[0] for x, v in enumerate(positive)], [v[1] for x, v in enumerate(positive)], c='r', marker='+') 
@@ -14,5 +15,18 @@ plt.scatter([v[0] for x, v in enumerate(negative)], [v[1] for x, v in enumerate(
 plt.xlabel('Exam 1 score')
 plt.ylabel('Exam 2 score')
 # Show the plot 
-plt.show() 
+#plt.show() 
 
+#init variables
+X = test.data[:, :-1]
+Y = test.data[:, -1]
+ones_X = np.hstack((np.ones((len(X), 1)), X))
+initial_theta = np.zeros((len(X[0]) + 1, 1))
+#compute cost and gradient using cost function.
+cost, grad = test.costFunction(initial_theta, ones_X, Y)
+#print(cost, grad)
+#plot decision boundary
+x_values = [np.min(X[:, 1] -3 ), np.max(X[:, 1] +3 )]
+y_values = np.matmul((-1./grad[2]), (np.matmul(np.array(grad[1]).reshape((1, 1)),np.transpose(np.array(x_values).reshape((len(x_values), 1)))) + grad[0]))
+plt.plot(x_values, -y_values[::-1], 'g-')
+plt.show()
