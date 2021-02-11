2766965b9428d54f714c637c6d8b07b58b4e69eb
Javiar Titus <javiar.titus@amac02w42jxhtd8.fios-router.home>
2021-02-10 23:37:32 -0500

    updates

--- logisticRegressionModels/logisticRegression.py
+++ logisticRegressionModels/logisticRegression.py
@@ -4,28 +4,31 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import math
+from numpy import sin, cos, pi
+from scipy.optimize import leastsq
 
-class logisticRegression():
-    #Given a data set of home features and home prices this class will predict the price of homes
-    # given the features home size and number of bedrooms.
+class logistic_regression():
+    #Given a data set of test scores for students this class will predict the probability that student gets
+    #admitted.
     def __init__(self):
         with open('ex2data1.txt') as f:
             data = f.read()
-        
+        with open('ex2data2.txt') as f:
+            data2 = f.read()
         self.data = np.array(re.split(',|\n', data)).reshape((100, 3)).astype(np.float)
+        self.data2 = np.array(re.split(',|\n', data2)).reshape((118, 3)).astype(np.float)
 
     def sigmoid(self, z):
         #returns a matrix or array where every value is "activated" with the sigmoid function
         rows, cols = z.shape
-
         g = np.zeros((rows, cols))
         for i in range(rows):
             for j in range(cols):
-                g[i, j] = 1/(1 + math.exp(-z[i, j]))
+                g[i, j] = (1)/(1 + np.exp(-z[i, j]))
         
         return g
     
-    def costFunction(self, theta, X, Y):
+    def cost_function(self, theta, X, Y):
         #Compute cost and gradient for logistic regression
         m = len(Y); # number of training examples
         f = len(X[0])
@@ -45,6 +48,106 @@ class logisticRegression():
                 temp[j, 0] += ((self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1)) - Y[i]) * X[i, j])
             
         for i in range(f):
-            grad[i, 0] = temp[i, 0] / m
+            grad[i, 0] = round(temp[i, 0] / m, 4)
         
         return cost, grad
+
+    def predict(self, theta, X):
+        #predict admission probability
+
+        m = len(X)
+        f = len(X[0])
+        p = 0
+        for i in range(m):
+            if self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :]).reshape((f, 1)))) >= 0.5:
+                p += 1
+        
+        return p / m
+
+    def cost_function_reg(self, theta, X, Y, lamb):
+        #Compute regularized cost and gradient for logistic regression
+        m = len(Y); # number of training examples
+        f = len(X[0])
+        cost = 0
+        grad = np.zeros((theta.size, 1))
+
+        for i in range(m):
+            cost += -Y[i] * math.log(self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1))) - (1 - Y[i]) * np.log(1 - self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1)))
+
+        reg = 0
+        for i in range(1, f):
+            reg += theta[i, 0] ** 2
+        
+        reg *= lamb / (2 * m)
+        cost = cost / m + reg
+
+        temp = np.zeros((f, 1))
+
+        for i in range(m):
+            for j in range(f):
+                
+                temp[j, 0] += ((self.sigmoid(np.matmul(np.transpose(theta), np.transpose(X[i, :])).reshape(1, 1)) - Y[i]) * X[i, j])
+            
+        for i in range(f):
+            if i > 1:
+                grad[i, 0] = round((temp[i, 0] / m) + (lamb / m) * theta[i, 0], 4)
+            else:
+                grad[i, 0] = round(temp[i, 0] / m, 4)
+
+        return cost, grad
+    
+    def map_feature(self, f1, f2):
+        #maps the two input features
+        #to quadratic features used in the regularization exercise.
+        #f1 and f2 must be the same size
+        degree = 6
+        out = np.ones((len(f1), len(f1[0])))
+        for i in range(1, degree + 1):
+            for j in range(i + 1):
+                out = np.hstack((out, np.around(np.multiply(np.power(f1, i-j), np.power(f2, j)), 6)))
+        
+        return out
+    
+    def find_boundary(self, x, y, n, plot_pts=1000):
+
+        def sines(theta):
+            ans = np.array([sin(i*theta)  for i in range(n+1)])
+            return ans
+
+        def cosines(theta):
+            ans = np.array([cos(i*theta)  for i in range(n+1)])
+            return ans
+
+        def residual(params, x, y):
+            x0 = params[0]
+            y0 = params[1]
+            c = params[2:]
+            r_pts = ((x-x0)**2 + (y-y0)**2)**0.5
+
+            thetas = np.arctan2((y-y0), (x-x0))
+            m = np.vstack((sines(thetas), cosines(thetas))).T
+            r_bound = m.dot(c)
+
+            delta = r_pts - r_bound
+            delta[delta>0] *= 10
+
+            return delta
+
+        # initial guess for x0 and y0
+        x0 = np.mean(x)
+        y0 = np.mean(y)
+        params = np.zeros(2 + 2*(n+1))
+
+        params[0] = x0
+        params[1] = y0
+        params[2:] += 1000
+        popt, pcov = leastsq(residual, x0=params, args=(x, y),
+                            ftol=1.e-12, xtol=1.e-12)
+        thetas = np.linspace(0, 2*pi, plot_pts)
+        m = np.vstack((sines(thetas), cosines(thetas))).T
+        c = np.array(popt[2:])
+        r_bound = m.dot(c)
+        x_bound = popt[0] + r_bound*cos(thetas)
+        y_bound = popt[1] + r_bound*sin(thetas)
+
+        return x_bound, y_bound
--- logisticRegressionModels/logisticRegressionTest.py
+++ logisticRegressionModels/logisticRegressionTest.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from logisticRegression import logisticRegression
+from logisticRegression import logistic_regression
 
-test = logisticRegression()
+test = logistic_regression()
 #original data plot
 fig = plt.figure(figsize=(10,7))
 positive = [v[:-1] for x, v in enumerate(test.data) if test.data[x][2]]
@@ -23,10 +23,54 @@ Y = test.data[:, -1]
 ones_X = np.hstack((np.ones((len(X), 1)), X))
 initial_theta = np.zeros((len(X[0]) + 1, 1))
 #compute cost and gradient using cost function.
-cost, grad = test.costFunction(initial_theta, ones_X, Y)
-#print(cost, grad)
+cost, grad = test.cost_function(initial_theta, ones_X, Y)
+#print("cost: ", cost)
+#print("grad:" , grad)
 #plot decision boundary
-x_values = [np.min(X[:, 1] -3 ), np.max(X[:, 1] +3 )]
+x_values = [np.min(X[:, 1] -10 ), np.max(X[:, 1] +10 )]
 y_values = np.matmul((-1./grad[2]), (np.matmul(np.array(grad[1]).reshape((1, 1)),np.transpose(np.array(x_values).reshape((len(x_values), 1)))) + grad[0]))
 plt.plot(x_values, -y_values[::-1], 'g-')
+plt.show()
+
+#predict admission for student with following test scores
+theta = np.array([-25.1613, 0.2062, 0.2015]).reshape((3, 1))
+prob = test.sigmoid(np.matmul(np.array([1,45, 85]).reshape((1, 3)), theta))
+#print("probability of sample student: ", prob[0][0])
+
+#compute accuracy on entire training set
+p = test.predict(theta, ones_X) 
+#print("model accuracy: ", p)
+
+#second plot
+fig = plt.figure(figsize=(10,7))
+positive = [v[:-1] for x, v in enumerate(test.data2) if test.data2[x][2]]
+negative = [v[:-1] for x, v in enumerate(test.data2) if test.data2[x][2] == 0]
+plt.scatter([v[0] for x, v in enumerate(positive)], [v[1] for x, v in enumerate(positive)], c='r', marker='+') 
+plt.scatter([v[0] for x, v in enumerate(negative)], [v[1] for x, v in enumerate(negative)], c='b', marker='_')
+plt.xlabel('Exam 1 score')
+plt.ylabel('Exam 2 score')
+# Show the plot 
+#plt.show() 
+
+#initialize params for second data set
+X = test.data2[:, :-1]
+Y = test.data2[:, -1]
+ones_X = np.hstack((np.ones((len(X), 1)), X))
+initial_theta = np.zeros((len(X[0]) + 1, 1))
+#create more features
+newX = test.map_feature(X[:, 0].reshape(118, 1), X[:, 1].reshape(118, 1))
+new_theta = np.zeros((len(newX[0]), 1))
+#compute reg cost
+cost, grad = test.cost_function_reg(new_theta, newX, Y, 1)
+#print("cost: ", cost[0])
+#print("grad:" , grad)
+
+#plot decision boundary around positive examples
+x, y = test.find_boundary(np.array(positive)[:, 0], np.array(positive)[:, 1], 7)
+plt.plot(x, y, '-k',c='g', lw=2.)
+
+#calculate model accuracy
+p = test.predict(new_theta, newX) 
+#print("model accuracy: ", p)
+
 plt.show()
